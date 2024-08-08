db9e479e1a1fa3873ed985c12fab9299212d2986
fmonroy75 <83427525+fmonroy75@users.noreply.github.com>
2024-08-07 22:20:22 -0400

    Update word_count.py, separa palabras y caracteres

--- word_count.py
+++ word_count.py
@@ -5,5 +5,12 @@ with open(sys.argv[1]) as file:
     #caracteres distintos
     caract_distintos = len(set(texto))
 
+
+
+    #separando palabras
+    arreglo_texto=texto.split(" ")
+    palabras_distintas=len(set(arreglo_texto))
     
+    print(f"El número de caracteres distintos es: {caract_distintos}")
+    print(f"El número de palabras distintas es: {palabras_distintas}")        
     
