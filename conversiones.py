b8e8e597e456a05b655803b7ab1c491bcb7fd4ff
Fish Nat <studyestudiante@gmail.com>
2024-08-08 18:41:53 -0400

    introduccion desafio conversion

---
+++ conversiones.py
@@ -0,0 +1,12 @@
+# Desafio Conversión 
+# sys.argv -> Sol, Peso Argentino, Dólar Americano 
+# ● a Sol peruano: 0.0046
+# ● a Peso Argentino: 0.093
+# ● a Dólar Americano: 0.00013
+
+# python conversiones.py 0.0046 0.093 0.0013 10000
+# Los 10000 pesos equivalen a:
+# 46.0 Soles
+# 930.0 Pesos Argentinos
+# 13.0 Dólares
+
